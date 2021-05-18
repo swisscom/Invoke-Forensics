4c13ed9520e0ebde2ffb3722c16812a15672b3f4
Andrew Rathbun <andrew.d.rathbun@gmail.com>
2021-05-18 16:37:52 -0400

    Delete Invoke-Forensic.ps1

--- Invoke-Forensic.ps1
+++
@@ -1,4 +0,0 @@
-# Module path for all functions
-$ModuleRoot = $PSScriptRoot
-# Load all functions
-Get-ChildItem -Path $ModuleRoot\* -Exclude "Invoke-Forensic.ps1" -Filter *.ps1 | % { . $_.FullName}
